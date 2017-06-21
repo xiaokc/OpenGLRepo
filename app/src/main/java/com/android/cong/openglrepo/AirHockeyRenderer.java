082437ad1a4e4f1b6bc29608c9f6bcfcdc344fca
xiaokecong <xiaokecong@baidu.com>
2017-06-21 16:55:48 +0800

    OpenGL ES 2.0 for Android
    first commit

---
+++ app/src/main/java/com/android/cong/openglrepo/AirHockeyRenderer.java
@@ -0,0 +1,127 @@
+package com.android.cong.openglrepo;
+
+import static android.opengl.GLES20.GL_FLOAT;
+import static android.opengl.GLES20.GL_LINES;
+import static android.opengl.GLES20.GL_POINTS;
+import static android.opengl.GLES20.GL_TRIANGLE_FAN;
+import static android.opengl.GLES20.glDrawArrays;
+import static android.opengl.GLES20.glEnableVertexAttribArray;
+import static android.opengl.GLES20.glGetAttribLocation;
+import static android.opengl.GLES20.glUseProgram;
+import static android.opengl.GLES20.glVertexAttribPointer;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import com.android.cong.openglrepo.util.LoggerConfig;
+import com.android.cong.openglrepo.util.ShaderHelper;
+import com.android.cong.openglrepo.util.TextResourceReader;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+
+/**
+ * Created by xiaokecong on 20/06/2017.
+ */
+
+public class AirHockeyRenderer implements GLSurfaceView.Renderer {
+    private static final int POSITION_COMPONENT_COUNT = 2;
+    private static final int BYTES_PER_FLOAT = 4;
+    private final FloatBuffer vertexData;
+    private final Context context;
+
+    private int program;
+
+    private static final String A_POSITION = "a_Position";
+    private int aPositionLocation;
+
+    private static final String A_COLOR = "a_Color";
+    private static final int COLOR_COMPONENT_COUNT = 3;
+    private static final int STRIDE = (POSITION_COMPONENT_COUNT + COLOR_COMPONENT_COUNT) * BYTES_PER_FLOAT;
+    private int aColorPosition;
+
+    public AirHockeyRenderer(Context context) {
+        this.context = context;
+        float[] tableVerticesWithTriangles = {
+                // Order of coordinates: X, Y, R, G, B
+                // Triangle Fan
+                0f, 0f, 1f, 1f, 1f,
+                -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
+                0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
+                0.5f, 0.5f, 0.7f, 0.7f, 0.7f,
+                -0.5f, 0.5f, 0.7f, 0.7f, 0.7f,
+                -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
+
+                // Line 1
+                -0.5f, 0f, 1f, 0f, 0f,
+                0.5f, 0f, 1f, 0f, 0f,
+
+                // Mallets
+                0f, -0.25f, 0f, 0f, 1f,
+                0f, 0.25f, 1f, 0f, 0f
+
+        };
+
+        vertexData = ByteBuffer.allocateDirect(tableVerticesWithTriangles.length * BYTES_PER_FLOAT)
+                .order(ByteOrder.nativeOrder())
+                .asFloatBuffer();
+        vertexData.put(tableVerticesWithTriangles); // 将数据从虚拟机内存拷贝到本地内存
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        gl.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        String vertexShaderSource = TextResourceReader.readTextFileFromResource(context, R.raw.simple_vertex_shader);
+        String fragmentShaderSource =
+                TextResourceReader.readTextFileFromResource(context, R.raw.simple_fragment_shader);
+
+        int vertexShader = ShaderHelper.compileVertexShader(vertexShaderSource);
+        int fragmentShader = ShaderHelper.compileFragmentShader(fragmentShaderSource);
+
+        program = ShaderHelper.linkProgram(vertexShader, fragmentShader);
+
+        if (LoggerConfig.ON) {
+            ShaderHelper.validateProgram(program);
+        }
+
+        glUseProgram(program);
+
+        aColorPosition = glGetAttribLocation(program, A_COLOR);
+        aPositionLocation = glGetAttribLocation(program, A_POSITION);
+
+        vertexData.position(0);
+        glVertexAttribPointer(aPositionLocation, POSITION_COMPONENT_COUNT, GL_FLOAT, false, STRIDE, vertexData);
+        glEnableVertexAttribArray(aPositionLocation);
+
+        vertexData.position(POSITION_COMPONENT_COUNT);
+        glVertexAttribPointer(aColorPosition, COLOR_COMPONENT_COUNT, GL_FLOAT, false, STRIDE, vertexData);
+        glEnableVertexAttribArray(aColorPosition);
+
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        gl.glViewport(0, 0, width, height);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+
+        glDrawArrays(GL_TRIANGLE_FAN, 0, 6);
+
+        // Draw the center dividing line.
+        glDrawArrays(GL_LINES, 6, 2);
+
+        // Draw the first mallet blue.
+        glDrawArrays(GL_POINTS, 8, 1);
+
+        // Draw the second mallet red.
+        glDrawArrays(GL_POINTS, 9, 1);
+    }
+
+}
---
+++ app/src/main/java/com/android/cong/openglrepo/util/LoggerConfig.java
@@ -0,0 +1,9 @@
+package com.android.cong.openglrepo.util;
+
+/**
+ * Created by xiaokecong on 20/06/2017.
+ */
+
+public class LoggerConfig {
+    public static final boolean ON = true;
+}
